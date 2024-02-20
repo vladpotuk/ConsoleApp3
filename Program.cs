a80e4625cfa28c441040e056aab67db927631140
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:31:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 123456;
+        int reversedNumber = ReverseNumber(number);
+        Console.WriteLine("Результат: " + reversedNumber);
+    }
+    static int ReverseNumber(int number)
+    {
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
+        }
+
+        return reversedNumber;
+    }
+}
